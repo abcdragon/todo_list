0ffa04ec08e7907d342bf22b714106af38ee7774
abcdragon <heybrostop@gmail.com>
2020-12-25 14:59:46 +0900

    할 일 목록 위젯이다.

---
+++ TodoListWidget.py
@@ -0,0 +1,28 @@
+from PyQt5.QtWidgets import QScrollArea, QWidget
+from PyQt5.QtWidgets import QVBoxLayout
+from PyQt5.QtCore import Qt
+
+from TodoListWidgetItem import TodoListWidgetItem
+
+
+class TodoListWidget(QScrollArea):
+    def __init__(self):
+        super(TodoListWidget, self).__init__()
+
+        self.todo_lst = QWidget()
+        self.todo_lst.setLayout(QVBoxLayout())
+        self.todo_lst.layout().setAlignment(Qt.AlignTop)
+
+        self.setWidget(self.todo_lst)
+        self.setWidgetResizable(True)
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+
+        self.setStyleSheet('background-color:"white"')
+
+    def add_todo(self, text, start_datetime=None, end_datetime=None):
+        item = TodoListWidgetItem(text)
+        self.todo_lst.layout().addWidget(item)
+
+    def rmv_todo(self, idx):
+        pass

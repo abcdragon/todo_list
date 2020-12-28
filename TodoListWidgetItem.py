178a58c3d7ae73d7fc99df088e1a81355bc0e483
abcdragon <heybrostop@gmail.com>
2020-12-29 02:22:42 +0900

    필요 없는 import 삭제 및 위젯 단순화

--- TodoListWidgetItem.py
+++ TodoListWidgetItem.py
@@ -3,17 +3,17 @@ from PyQt5.QtWidgets import QHBoxLayout
 
 from ResizedQLabel import ResizedQLabel
 
-from datetime import datetime
-
 
 class TodoListWidgetItem(QGroupBox):
-    def __init__(self, text):
+    def __init__(self, text, end_time):
         super(TodoListWidgetItem, self).__init__()
 
         self.setLayout(QHBoxLayout())
         self.__todo_label = ResizedQLabel(text)
         self.layout().addWidget(self.__todo_label)
-        self.layout().addWidget(ResizedQLabel('기간'))
+        self.layout().addStretch(1)
+        self.layout().addWidget(ResizedQLabel('| 기간:', 10))
+        self.layout().addWidget(ResizedQLabel(end_time, 10))
 
         self.setStyleSheet('''
             QGroupBox {
@@ -28,6 +28,6 @@ if __name__ == '__main__':
     from PyQt5.QtWidgets import QApplication
 
     qApp = QApplication(sys.argv)
-    todolistwidgetitem = TodoListWidgetItem('안녕')
+    todolistwidgetitem = TodoListWidgetItem('안녕', '2020-12-27')
     todolistwidgetitem.show()
     sys.exit(qApp.exec_())

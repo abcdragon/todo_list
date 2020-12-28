c5796188d4e5dcd54558025d34e9076ce2616823
abcdragon <heybrostop@gmail.com>
2020-12-29 02:20:40 +0900

    기간 입력하는 다이얼로그 클래스 정의

--- TodoEditWidget.py
+++ TodoEditWidget.py
@@ -1,10 +1,61 @@
-from PyQt5.QtWidgets import QWidget, QLineEdit, QPushButton
-from PyQt5.QtWidgets import QHBoxLayout
+from PyQt5.QtWidgets import QWidget, QLineEdit, QPushButton, QDialog, QDialogButtonBox
+from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout, QGroupBox
 from PyQt5.QtGui import QFont
+from PyQt5.QtCore import Qt
 
 from ResizedQLabel import ResizedQLabel
 
 from re import fullmatch
+
+
+class GetTermDialog(QDialog):
+    def __init__(self):
+        super(GetTermDialog, self).__init__()
+
+        self.setWindowFlag(Qt.WindowContextHelpButtonHint, False)
+        self.setWindowTitle('종료 날짜 입력')
+        self.setLayout(QVBoxLayout())
+        self.__init_ui()
+
+    def __init_ui(self):
+        info_box = QGroupBox('종료 날짜')
+        info_box.setLayout(QVBoxLayout())
+
+        info_box.layout().addWidget(ResizedQLabel('년-월-일:', 14))
+
+        input_layout = QHBoxLayout()
+        self.__ymd = [QLineEdit(), ResizedQLabel(' - ', 12), QLineEdit(), ResizedQLabel(' - ', 12), QLineEdit()]
+        for idx, widget in enumerate(self.__ymd):
+            if idx % 2 == 0:
+                widget.setFont(QFont('NanumGothic', 12))
+                widget.setMaxLength(4 if idx == 0 else 2)
+
+            input_layout.addWidget(widget)
+
+        self.__ymd = self.__ymd[::2]
+        info_box.layout().addLayout(input_layout)
+
+        btn_box = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+
+        btn_box.accepted.connect(self.__ok)
+        btn_box.rejected.connect(self.__reject)
+
+        self.layout().addWidget(info_box)
+        self.layout().addWidget(btn_box)
+
+    def get_end_time(self):
+        if [0 for e in self.__ymd if not e.text()] and self.__success:
+            raise ValueError('모든 칸을 입력해주세요')
+
+        return '-'.join([e.text() for e in self.__ymd]) if self.__success else None
+
+    def __ok(self):
+        self.__success = True
+        self.close()
+
+    def __reject(self):
+        self.__success = False
+        self.close()
 
 
 class TodoEditWidget(QWidget):

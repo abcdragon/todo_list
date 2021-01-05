37c7aacefdc9b4feda61670350b8c7b1ad0cfb7e
abcdragon <heybrostop@gmail.com>
2021-01-05 21:07:05 +0900

    설정한 텍스트 수정

--- TodoListWidgetItem.py
+++ TodoListWidgetItem.py
@@ -1,7 +1,7 @@
-from PyQt5.QtWidgets import QGroupBox
+from PyQt5.QtWidgets import QGroupBox, QLineEdit
 from PyQt5.QtWidgets import QHBoxLayout
 
-from ResizedQLabel import ResizedQLabel
+from ResizedQLabel import ResizedQLabel, template_font
 
 
 class TodoListWidgetItem(QGroupBox):
@@ -10,10 +10,11 @@ class TodoListWidgetItem(QGroupBox):
 
         self.setLayout(QHBoxLayout())
         self.__todo_label = ResizedQLabel(text)
+        self.__todo_label.mouseDoubleClickEvent = self.todo_double_clicked
         self.layout().addWidget(self.__todo_label)
         self.layout().addStretch(1)
-        self.layout().addWidget(ResizedQLabel('| 기간:', 10))
-        self.layout().addWidget(ResizedQLabel(end_time, 10))
+        self.layout().addWidget(ResizedQLabel('| 기간:', font_size=10))
+        self.layout().addWidget(ResizedQLabel(end_time, font_size=10))
 
         self.setStyleSheet('''
             QGroupBox {
@@ -21,6 +22,22 @@ class TodoListWidgetItem(QGroupBox):
                 border-style: solid;
             }
         ''')
+
+    def todo_double_clicked(self, event):
+        self.layout().removeWidget(self.__todo_label)
+        self.__todo_label.deleteLater()
+        self.__todo_label = QLineEdit(self.__todo_label.text())
+        self.__todo_label.focusOutEvent = self.focus_out
+        self.__todo_label.setFont(template_font())
+        self.layout().insertWidget(0, self.__todo_label)
+        self.__todo_label.setFocus()
+
+    def focus_out(self, event):
+        self.layout().removeWidget(self.__todo_label)
+        self.__todo_label.deleteLater()
+        self.__todo_label = ResizedQLabel(self.__todo_label.text())
+        self.__todo_label.mouseDoubleClickEvent = self.todo_double_clicked
+        self.layout().insertWidget(0, self.__todo_label)
 
 
 if __name__ == '__main__':
